fe58b51fa5081f3fd664354a173f34f824bdb2df
Marcelo Menezes <menezes_07@hotmail.com>
2015-06-20 14:55:24 -0300

    Update and rename ControllerPagamento to ControllerPagamento.java

---
+++ ControllerPagamento.java
@@ -0,0 +1,10 @@
+ @author Aluno
+ */
+public class ControllerPagamento {
+ public double calcularSalario(Funcionario docente, double qtHora) {
+		
+	double salario = docente.calcularSalario(qtHora);
+		
+	return salario;
+	}
+}
